25513ab6ec3b164eee74cd243c02bba8083ebfeb
kazimhr <53475165+kazimhr@users.noreply.github.com>
2021-06-01 17:22:45 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dont take panga with king KAZA
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          KAZA KING
         </a>
       </header>
     </div>
